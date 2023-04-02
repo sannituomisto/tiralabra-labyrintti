0763604cb28c4a9320928136d330ef35682bd84b
Sanni Tuomisto <sanni.tuomisto@helsinki.fi>
2023-04-02 17:19:20 +0300

    Muokataan index.py

--- src/index.py
+++ src/index.py
@@ -1,24 +1,8 @@
-from labyrintin_hakeminen import labyrintin_haku
-import ui
+from sovellus import Sovellus
 
 def main():
-    while True:
-        """Aloitus"""
-        ui.aloitus()
-        """Labyrintin mittojen syöttö"""
-        labyrintin_nro = ui.labyrintin_nro()
-        if labyrintin_nro == False:
-            continue
-        """Labyrintin teko"""
-        labyrintti=labyrintin_haku(labyrintin_nro)
-        if labyrintin_nro <= 5:
-            koko=20
-        else:
-            koko=100
-        # 150x150 kokoiset labyrintit, jos tulevat sovellukseen
-        # else:
-        #     koko=150
-        ui.labyrintti(labyrintti,koko)
+    sovellus = Sovellus()
+    sovellus.suorita()
 
 if __name__ == '__main__':
     main()
