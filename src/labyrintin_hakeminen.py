100d08c8e49e8675a2845ca0e2d1512b3b599bfc
Sanni Tuomisto <sanni.tuomisto@helsinki.fi>
2023-04-02 17:20:02 +0300

    Muokataan labyrinttitiedostojen käsittelyä

--- src/labyrintin_hakeminen.py
+++ src/labyrintin_hakeminen.py
@@ -1,23 +1,22 @@
 def labyrintin_haku(labyrintin_nro):
+        labyrintti=[]
         if labyrintin_nro <= 5:
                 with open("20x20labyrintit/labyrintti"+str(labyrintin_nro)+".txt") as tiedosto:
-                        labyrintti=[]
                         for rivi in tiedosto:
                                 rivi = rivi.replace("\n", "")
-                                labyrintti.append(rivi)
+                                labyrintti.append(list(rivi))
         else:
                 with open("100x100labyrintit/labyrintti"+str(labyrintin_nro)+".txt") as tiedosto:
-                        labyrintti=[]
                         for rivi in tiedosto:
                                 rivi = rivi.replace("\n", "")
-                                labyrintti.append(rivi)
+                                labyrintti.append(list(rivi))
         return labyrintti
         
         # Ei näy terminaalissa järkevästi, joten 150x150 labyrintit ei välttämättä tule sovellukseen. Toisaalta aikavertailussa olisi hyviä.
         # else:
         #         with open("150x150labyrintit/labyrintti"+str(labyrintin_nro)+".txt") as tiedosto:
-        #                 labyrintti=[]
         #                 for rivi in tiedosto:
         #                         rivi = rivi.replace("\n", "")
-        #                         labyrintti.append(rivi)
+        #                         labyrintti.append(list(rivi))
         # return labyrintti
+
