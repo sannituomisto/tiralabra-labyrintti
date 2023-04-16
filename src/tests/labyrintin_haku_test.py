61911c3df7bd5c90739e0d2e7c70ee5cf1653709
Sanni Tuomisto <sanni.tuomisto@helsinki.fi>
2023-04-16 21:33:24 +0300

    Poistetaan turha testi

--- src/tests/labyrintin_haku_test.py
+++ src/tests/labyrintin_haku_test.py
@@ -12,9 +12,3 @@ class TestLabyrintinHaku(unittest.TestCase):
 
         self.assertEqual(len(labyrintti[0]), 20)
         self.assertEqual(len(labyrintti), 20)
-
-    def test_100x100_labyrintin_haku_onnistuu(self):
-        labyrintti = labyrintin_haku(self.labyrintin_nro2)
-
-        self.assertEqual(len(labyrintti[0]), 100)
-        self.assertEqual(len(labyrintti), 100)
