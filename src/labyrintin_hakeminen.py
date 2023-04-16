3dcb612055d5fb36657191a3ff11e505e6823c57
Sanni Tuomisto <sanni.tuomisto@helsinki.fi>
2023-04-16 21:29:11 +0300

    Poistetaan turhaa koodia

--- src/labyrintin_hakeminen.py
+++ src/labyrintin_hakeminen.py
@@ -17,18 +17,5 @@ def labyrintin_haku(labyrintin_nro):
             for rivi in tiedosto:
                 rivi = rivi.replace("\n", "")
                 labyrintti.append(list(rivi))
-    else:
-        with open("100x100labyrintit/labyrintti"+str(labyrintin_nro)+".txt", encoding="utf-8") as tiedosto:
-            for rivi in tiedosto:
-                rivi = rivi.replace("\n", "")
-                labyrintti.append(list(rivi))
+    # Tähän tulee mahdollisesti vielä isompien labyrinttien hakeminen.
     return labyrintti
-
-    # Ei näy terminaalissa järkevästi, joten 150x150 labyrintit ei välttämättä tule sovellukseen.
-    # Toisaalta aikavertailussa olisi hyviä.
-    # else:
-    #         with open("150x150labyrintit/labyrintti"+str(labyrintin_nro)+".txt") as tiedosto:
-    #                 for rivi in tiedosto:
-    #                         rivi = rivi.replace("\n", "")
-    #                         labyrintti.append(list(rivi))
-    # return labyrintti
