0b9583cf97a1ffd3e94c72f573af05741778a291
Sanni Tuomisto <sanni.tuomisto@helsinki.fi>
2023-04-23 22:02:53 +0300

    Poistetaan projektin vanhan version tiedostot

--- src/front_end/vertaile.py
+++
@@ -1,8 +0,0 @@
-"""Algoritmien vertailusta vastaava moduuli käyttöliittymän puolella. Moduuli siis täysin keskeneräinen.
-"""
-
-
-def vertaile_algoritmeja():
-    """Funktio kesken.
-    """
-    print('Keskeneräinen toiminto.')
