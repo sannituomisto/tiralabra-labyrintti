07f7b14342e9e9ae6409ab1dab9b6a0ad601bba2
Sanni Tuomisto <sanni.tuomisto@helsinki.fi>
2023-04-23 23:18:59 +0300

    Päivitetään dead end filling algoritmin testejä

--- src/tests/dead_end_filling_test.py
+++ src/tests/dead_end_filling_test.py
@@ -1,34 +1,33 @@
 import unittest
-from unittest.mock import Mock, ANY
+from unittest.mock import Mock
 from dead_end_filling import DeadEndFilling
-from ikkuna import PygameIkkuna
 
 
 class TestDeadEndFilling(unittest.TestCase):
     def setUp(self):
-        self.labyrintti = [['@', '.', '@', '@', '@', '@', '@', '@', '@', '@'],
-                           ['@', '.', '@', '@', '.', '.', '.', '.', '.', '@'],
-                           ['@', '.', '.', '@', '.', '@', '@', '@', '.', '@'],
-                           ['@', '.', '@', '@', '.', '@', '.', '@', '@', '@'],
-                           ['@', '.', '.', '.', '.', '.', '.', '.', '.', '@'],
-                           ['@', '.', '@', '@', '@', '.', '@', '@', '@', '@'],
-                           ['@', '.', '@', '.', '.', '.', '.', '.', '.', '@'],
-                           ['@', '.', '@', '@', '@', '@', '@', '@', '.', '@'],
-                           ['@', '.', '.', '.', '.', '.', '.', '.', '.', '@'],
-                           ['@', '@', '@', '@', '@', '@', '@', '@', '.', '@']]
-        self.koko = 10
-        self.ikkuna_mock = Mock()
+        self.labyrinth = [['@', '.', '@', '@', '@', '@', '@', '@', '@', '@'],
+                          ['@', '.', '@', '@', '.', '.', '.', '.', '.', '@'],
+                          ['@', '.', '.', '@', '.', '@', '@', '@', '.', '@'],
+                          ['@', '.', '@', '@', '.', '@', '.', '@', '@', '@'],
+                          ['@', '.', '.', '.', '.', '.', '.', '.', '.', '@'],
+                          ['@', '.', '@', '@', '@', '.', '@', '@', '@', '@'],
+                          ['@', '.', '@', '.', '.', '.', '.', '.', '.', '@'],
+                          ['@', '.', '@', '@', '@', '@', '@', '@', '.', '@'],
+                          ['@', '.', '.', '.', '.', '.', '.', '.', '.', '@'],
+                          ['@', '@', '@', '@', '@', '@', '@', '@', '.', '@']]
+        self.size = 10
+        self.pygame_mock = Mock()
         self.DeF = DeadEndFilling(
-            self.labyrintti, 10, 60, ANY, self.ikkuna_mock)
+            self.labyrinth, self.pygame_mock, self.size, is_test=True)
 
-    def test_loytaa_umpikujat(self):
-        umpikujat = self.DeF.etsi_umpikujat()
-        vastaus = [(2, 2), (2, 8), (3, 6), (6, 3), (4, 8)]
-        self.assertEqual(sorted(umpikujat), sorted(vastaus))
+    def test_find_dead_ends(self):
+        dead_ends = self.DeF.find_dead_ends()
+        result = [(2, 2), (2, 8), (3, 6), (6, 3), (4, 8)]
+        self.assertEqual(sorted(dead_ends), sorted(result))
 
-    def test_osaa_tayttaa_umpikujat(self):
-        umpikujat = self.DeF.etsi_umpikujat()
-        umpikujien_kujat = self.DeF.taytetaan_umpikujat(umpikujat)
-        vastaus = [(2, 2), (2, 8), (1, 8), (1, 7), (1, 6), (1, 5), (1, 4),
-                   (2, 4), (3, 4), (3, 6), (4, 8), (4, 7), (4, 6), (6, 3), (6, 4)]
-        self.assertEqual(sorted(umpikujien_kujat), sorted(vastaus))
+    def test_fill_dead_ends(self):
+        dead_ends = self.DeF.find_dead_ends()
+        dead_end_paths = self.DeF.fill_dead_ends(dead_ends)
+        result = [(2, 2), (2, 8), (1, 8), (1, 7), (1, 6), (1, 5), (1, 4),
+                  (2, 4), (3, 4), (3, 6), (4, 8), (4, 7), (4, 6), (6, 3), (6, 4)]
+        self.assertEqual(sorted(dead_end_paths), sorted(result))
