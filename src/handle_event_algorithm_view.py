73d247d2170b8bf76fd9d8d2cf70da9d6e8832f3
Sanni Tuomisto <sanni.tuomisto@helsinki.fi>
2023-04-29 13:30:44 +0300

    Lisätään Pygame ikkunan tapahtumien käsittelyyn tarvittava funktio.

---
+++ src/handle_event_algorithm_view.py
@@ -0,0 +1,21 @@
+"""Pygame ikkunan tapahtumia algoritmien lopetettua.
+    """
+import sys
+import pygame
+
+
+def handle_event(event):
+    """Käsittelee algoritminäkymän tapahtumat, kun tremauxin algoritmi on päässyt loppuun.
+    Args:
+        event (pygame event): Pygame tapahtuma
+    """
+    if event.type == pygame.QUIT:
+        pygame.quit()
+        sys.exit()
+    elif event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_ESCAPE:
+            pygame.quit()
+            sys.exit()
+        elif event.key == pygame.K_m:
+            return True
+    return None
